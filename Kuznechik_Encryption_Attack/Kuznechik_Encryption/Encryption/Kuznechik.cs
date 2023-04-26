33262339b2ff253df4a17117f6be7538b7322f4e
Andrey <84859114+Andrey1039@users.noreply.github.com>
2023-04-26 10:20:52 +0500

    Добавлена программа

---
+++ Kuznechik_Encryption_Attack/Kuznechik_Encryption/Encryption/Kuznechik.cs
@@ -0,0 +1,230 @@
+﻿using System.Linq;
+using System.Text;
+using Kuznechik_Encryption.Data;
+using System.Collections.Generic;
+
+namespace Kuznechik_Encryption.Encryption
+{
+    internal class Kuznechik
+    {
+        // Умножение чисел в поле Галуа
+        private byte GF_Mul(byte a, byte b)
+        {
+            byte p = 0;
+
+            for (byte counter = 0; counter < 8 && a != 0 && b != 0; counter++)
+            {
+                if ((b & 1) != 0)
+                    p ^= a;
+
+                byte hi_bit_set = (byte)(a & 0x80);
+                a <<= 1;
+
+                if (hi_bit_set != 0)
+                    a ^= 0xc3;
+                b >>= 1;
+            }
+
+            return p;
+        }
+
+        // Сдвиг данных и реализация уравнения для L (прямая)
+        private byte[] R(byte[] data)
+        {
+            byte a = 0;
+
+            for (int i = 0; i < 16; i++)
+                a ^= GF_Mul(data[i], ConversionTables.L_Mass[i]);
+
+            for (int i = 15; i >= 0; i--)
+                if (i != 0)
+                    data[i] = data[i - 1];
+                else
+                    data[i] = a;
+
+            return data;
+        }
+
+        // Сдвиг данных и реализация уравнения для L (обратная)
+        private byte[] R_Reverse(byte[] data)
+        {
+            byte a = data[0];
+            byte[] newData = new byte[16];
+
+            for (int i = 0; i < 15; i++)
+                newData[i] = data[i + 1];
+
+            for (int i = 15; i >= 0; i--)
+                a ^= GF_Mul(newData[i], ConversionTables.L_Mass[i]);
+
+            newData[15] = a;
+
+            return newData;
+        }
+
+        // Побитовый XOR ключа и входного блока данных
+        public byte[] X(byte[] a, byte[] b)
+        {
+            byte[] output = new byte[16];
+
+            for (int i = 0; i < 16; i++)
+                output[i] = (byte)(a[i] ^ b[i]);
+
+            return output;
+        }
+
+        // Нелинейное преобразование
+        public byte[] S(byte[] data, bool encryption)
+        {
+            byte[] output = new byte[16];
+
+            for (int i = 0; i < data.Length; i++)
+                if (encryption)
+                    output[i] = ConversionTables.Pi[data[i]];
+                else
+                    output[i] = ConversionTables.Pi_Reverse[data[i]];
+
+            return output;
+        }
+
+        // Линейное преобразование
+        public byte[] L(byte[] data)
+        {
+            for (int i = 0; i < 16; i++)
+                data = R(data);
+
+            return data;
+        }
+
+        // Преобразование данных (шифрование)
+        private string XSL(byte[] partsData, List<byte[]> partsKey)
+        {
+            byte[] codedData = new byte[16];
+
+            for (int j = 0; j <= 8; j++)
+            {
+                codedData = X(partsKey[j], partsData);
+                codedData = S(codedData, true);               
+                codedData = L(codedData);
+
+                partsData = codedData;
+            }
+
+            codedData = X(partsKey[9], codedData);
+            string result = Encoding.GetEncoding(1251).GetString(codedData);
+
+            return result;
+        }
+
+        // Преобразование данных с внесением ошибки (шифрование)
+        private string XSL_Mod(byte[] partsData, List<byte[]> partsKey, int round, int numberByte)
+        {
+            byte[] codedData = new byte[16];
+
+            for (int j = 0; j <= 8; j++)
+            {
+                codedData = X(partsKey[j], partsData);
+                codedData = S(codedData, true);
+
+                if (j == round)
+                    codedData[numberByte] = 0;
+
+                codedData = L(codedData);
+                partsData = codedData;
+            }
+
+            codedData = X(partsKey[9], codedData);
+            string result = Encoding.GetEncoding(1251).GetString(codedData);
+
+            return result;
+        }
+
+        // Вычисление констант
+        private List<byte[]> C_Calculate()
+        {
+            List<byte[]> c = new List<byte[]>();
+
+            for (int i = 0; i < 32; i++)
+            {
+                c.Add(new byte[16]);
+                c[i][15] = (byte)(i + 1);
+                c[i] = L(c[i]);
+            }
+
+            return c;
+        }
+
+        // Сеть Фейстеля
+        private List<byte[]> Feistel(byte[] keyA, byte[] keyB, byte[] c)
+        {
+            List<byte[]> keys = new List<byte[]>
+            {
+                X(L(S(X(keyA, c), true)), keyB), 
+                keyA
+            };
+
+            return keys;
+        }
+
+        // Формирование списка ключей
+        private List<byte[]> GetKeys(string key)
+        {
+            List<byte[]> keys = new List<byte[]>();
+            List<byte[]> tempKeyA = new List<byte[]>();
+
+            List<byte[]> c = C_Calculate();
+
+            keys.Add(Encoding.GetEncoding(1251).GetBytes(key.Substring(0, key.Length / 2)));
+            keys.Add(Encoding.GetEncoding(1251).GetBytes(key.Substring(key.Length / 2, key.Length / 2)));
+
+            tempKeyA.Add(keys[0]);
+            tempKeyA.Add(keys[1]);
+
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 8; j += 2)
+                {
+                    List<byte[]> tempKeyB = Feistel(tempKeyA[0], tempKeyA[1], c[8 * i + j]);
+                    tempKeyA = Feistel(tempKeyB[0], tempKeyB[1], c[8 * i + j + 1]);
+                }
+
+                keys.Add(tempKeyA[0]);
+                keys.Add(tempKeyA[1]);
+            }
+
+            return keys;
+        }
+
+        // Формирование списка сообщений
+        private List<byte[]> GetMessage(string data)
+        {
+            while (data.Length % 16 != 0)
+                data += "\0";
+
+            byte[] byteText = Encoding.GetEncoding(1251).GetBytes(data);
+
+            int counter = 16;
+            List<byte[]> result = byteText.GroupBy(_ => counter++ / 16).Select(v => v.ToArray()).ToList();
+
+            return result;
+        }
+
+        // Шифрование сообщения
+        public string Encryption(string data, string key, bool withError, int round, int numberByte)
+        {
+            string result = string.Empty;
+            
+            List<byte[]> partsKey = GetKeys(key);
+            List<byte[]> partsData = GetMessage(data);
+
+            if (withError)
+                for (int i = 0; i < 1; i++)
+                    result += XSL_Mod(partsData[i], partsKey, round, numberByte);            
+            else
+                for (int i = 0; i < partsData.Count; i++)
+                    result += XSL(partsData[i], partsKey);
+
+            return result;
+        }
+    }
+}
---
+++ Kuznechik_Encryption_Attack/Kuznechik_Encryption/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using Kuznechik_Encryption.Data;
+using Kuznechik_Encryption.Encryption;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Kuznechik_Encryption
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            InstallParams();
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+        }
+
+        private void InstallParams()
+        {
+            InputTextTB.Text = "qwertyuiopasdfgh";
+            InputKeyTB.Text = "123456789ghtrewsdcvbnmkjhgfdsazx";
+        }
+
+        private void InputData_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (InputTextTB.Text.Equals(string.Empty) || InputKeyTB.Text.Length != 32)
+                AttackBtn.IsEnabled = false;
+            else
+                AttackBtn.IsEnabled = true;
+        }
+
+        private void AttackBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Kuznechik message = new Kuznechik();
+            byte[] resultKey = new byte[32];
+
+            for (int round = 0; round < 2; round++)
+            {
+                byte[] inputText = Encoding.GetEncoding(1251).GetBytes(InputTextTB.Text);
+
+                for (int numberByte = 0; numberByte < 16; numberByte++)
+                    for (int symbol = 0; symbol < 256; symbol++)
+                    {
+                        inputText[numberByte] = (byte)symbol;
+                        string modInputText = Encoding.GetEncoding(1251).GetString(inputText);
+                    
+                        string originalResult = message.Encryption(modInputText, InputKeyTB.Text, true, round, numberByte);
+                        string modResult = message.Encryption(modInputText, InputKeyTB.Text, false, round, numberByte);
+
+                        if (originalResult == modResult)
+                        {
+                            if (round == 0)
+                            {
+                                resultKey[numberByte] = ConversionTables.Pi_Reverse[0];
+                                resultKey[numberByte] = (byte)(resultKey[numberByte] ^ inputText[numberByte]);
+                            }
+                            else
+                            {
+                                byte[] secondRoundText = message.X(inputText, resultKey);
+                                secondRoundText = message.S(secondRoundText, true);
+                                secondRoundText = message.L(secondRoundText);
+
+                                resultKey[numberByte + 16] = ConversionTables.Pi_Reverse[0];
+                                resultKey[numberByte + 16] = (byte)(resultKey[numberByte + 16] ^ secondRoundText[numberByte]);
+                            }
+                        }
+                    }
+            }
+
+            FoundedKeyTB.Text = Encoding.GetEncoding(1251).GetString(resultKey);
+            ResultLB.Content = FoundedKeyTB.Text == InputKeyTB.Text ? "да" : "нет";               
+        }     
+    }
+}
